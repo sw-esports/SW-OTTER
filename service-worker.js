94d18a9c799d5a8d835024ee11d15af9b0c732ac
sw-esports <SW.ESPORTS.OFFICAL@gmail.com>
2025-01-18 17:00:41 +0530

    pwa

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'sw-otter-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/service.html',
+  '/blog.html',
+  '/contact.html',
+  '/css/style.css',
+  '/images/logo.png',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
